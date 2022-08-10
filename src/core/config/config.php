8d844cb6b79d31a8a9ea3ac04391abf38279fc12
Ali Dev <97373457+DevNull-IR@users.noreply.github.com>
2022-08-10 19:00:14 +0430

    fixed bugs

--- src/core/config/config.php
+++ src/core/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    "token" => "5508323245:AAElyZMAqMc5xC9E6CaT5ZqRj7W4wwpVQZU", // or Default
+    "token" => "Token", // or Default
     "channel_username" => "folan_channel", // or Default
     "channel_id" => -1001727995420, // or Default
     "viewer" => "assets/images",
